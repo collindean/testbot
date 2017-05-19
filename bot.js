eb7bcdcbda230c33c312bbf041defacddff1efc5
Collin Dean <collin@CollinLaptop.localdomain>
2017-05-19 07:43:26 -0500

    fixing bullshit

--- bot.js
+++ bot.js
@@ -72,5 +72,43 @@ console.log('sending ' + botResponse + ' to ' + botID);
   botReq.end(JSON.stringify(body));
 }
 
+function helpMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "can i help you?";
+  var hiResponse = "hi";
+  var helpResponse = "well i CAN help";
+  var brokeResponse = "congrats, you broke me";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : "hi"
+  };
+
+console.log('sending ' + helpResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 
 exports.respond = respond;
