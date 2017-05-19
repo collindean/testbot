d73a2a29af1735931345b56e19410f858896c373
Collin Dean <collin@CollinLaptop.localdomain>
2017-05-19 11:49:20 -0500

    fixing bullshit

--- bot.js
+++ bot.js
@@ -128,7 +128,7 @@ function countdownMessage() {
 
  /*    botReponse = daysLeft + " days, " + hoursLeft + " hours, " + minutesLeft + " minutes, and " + secondsLeft + " seconds left until next session.";
 */
-	botResponse = "butt";
+	botResponse = daysLeft;
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
