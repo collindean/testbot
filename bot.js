45980021aa64455409e0b3ba1b6e06c17579c2f5
Collin Dean <collin@CollinLaptop.localdomain>
2017-05-19 10:01:11 -0500

    fixing bullshit

--- bot.js
+++ bot.js
@@ -9,7 +9,8 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\hi bot$/,
       botHelp = /^\help$/,
-      botWeapon = "whatis";
+      botWeapon = "whatis",
+      botD12 = /^\roll$/;
       usrInput = request.text;
 
   if(request.text && botRegex.test(request.text)) {
@@ -27,12 +28,86 @@ function respond() {
     botAction = 3;
     whatisMessage();
     this.res.end();
-    } else {
+    } else if (request.text && botD12.test(request.text)) {
+    this.res.writeHead(200);
+    botAction = 4; 
+    rollD12();
+    this.res.end();
+  } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
+
+function rollD12() {
+var botResponse, options, body, botReq;
+var rollAttempt = Math.floor((Math.random()*12)+1);
+var rollTotal = rollAttempt;
+var rolled12 = false;
+if (rollAttempt <= 12) {
+	rolled12 = true;
+}
+
+while (rollAttempt = true) {
+rollAttempt = Math.floor((Math.random()*12)+1);
+rollTotal += rollAttempt;
+if (rollAttempt > 12) {
+	rolled12 = false
+}
+}
+
+var rollTurnout;
+
+if (rollAttempt >= 50) {
+	rollTurnout == "Tier 6, you won life.";
+} else if (rollAttempt >= 40 {
+	rollTurnout == "Tier 5, that's godlike!!!";
+} else if (rollAttempt >= 30 {
+	rollTurnout == "Tier 4, that's amazing!!";
+} else if (rollAttempt >= 20 { 
+	rollTurnout == "Tier 3, that's good!";
+} else if (rollAttempt >= 10 { 
+	rollTurnout == "Tier 2, you made it, just barely";
+} else if (rollAttempt >= 2 { 
+	rollTurnout == "Tier 1, you might be ok?";
+} else if (rollAttempt = 1 { 
+	rollTurnout == "lol you fucking suck";
+} else { rollTurnout == "i'm not sure what happened here";
+}
+
+botResponse = "You rolled a " + rollTotal + ". " + rollTurnout;
+
+options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+	
 
 function whatisMessage() {
 var botResponse, options, body, botReq;
@@ -41,7 +116,7 @@ var botResponse = "i don't know, you tell me";
 var troy = "gay";
 
 if (slicedText == "troy") {
-     botResponse = troy;
+     botResponse = "gay";
 } else if (slicedText == "vis") {
      botResponse = "Vis is a currency in this universe, which can be farmed/mined from many resources. It can be found in water, vains, and many other options that I don't know.";
 } else if (slicedText == "light armor") {
